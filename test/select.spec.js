e34bffc5335a8e2f1f547081af89169d98a17453
Teegrounet <nico210089@gmail.com>
2016-04-11 21:10:23 +0200

    Fix tagging for all, add tagging-equals function

--- test/select.spec.js
+++ test/select.spec.js
@@ -146,25 +146,31 @@ describe('ui-select tests', function() {
 
   function createUiSelect(attrs) {
     var attrsHtml = '',
-        matchAttrsHtml = '';
+        matchAttrsHtml = '',
+		choicesRepeatAttrsHtml = '',
+		choicesAttrsHtml = '';
     if (attrs !== undefined) {
+	  if (attrs.multiple !== undefined) { attrsHtml += ' multiple'; }
       if (attrs.disabled !== undefined) { attrsHtml += ' ng-disabled="' + attrs.disabled + '"'; }
       if (attrs.required !== undefined) { attrsHtml += ' ng-required="' + attrs.required + '"'; }
       if (attrs.theme !== undefined) { attrsHtml += ' theme="' + attrs.theme + '"'; }
       if (attrs.tabindex !== undefined) { attrsHtml += ' tabindex="' + attrs.tabindex + '"'; }
       if (attrs.tagging !== undefined) { attrsHtml += ' tagging="' + attrs.tagging + '"'; }
+	  if (attrs.taggingEquals !== undefined) { attrsHtml += ' tagging-equals="' + attrs.taggingEquals + '"'; }
       if (attrs.taggingTokens !== undefined) { attrsHtml += ' tagging-tokens="' + attrs.taggingTokens + '"'; }
       if (attrs.title !== undefined) { attrsHtml += ' title="' + attrs.title + '"'; }
       if (attrs.appendToBody !== undefined) { attrsHtml += ' append-to-body="' + attrs.appendToBody + '"'; }
       if (attrs.allowClear !== undefined) { matchAttrsHtml += ' allow-clear="' + attrs.allowClear + '"';}
       if (attrs.inputId !== undefined) { attrsHtml += ' input-id="' + attrs.inputId + '"'; }
       if (attrs.ngClass !== undefined) { attrsHtml += ' ng-class="' + attrs.ngClass + '"'; }
+	  if (attrs.groupBy !== undefined) { choicesAttrsHtml += ' group-by="' + attrs.groupBy + '"'; }
+	  if (attrs.trackBy !== undefined) { choicesRepeatAttrsHtml += ' track by ' + attrs.trackBy; }
     }
 
     return compileTemplate(
       '<ui-select ng-model="selection.selected"' + attrsHtml + '> \
         <ui-select-match placeholder="Pick one..."' + matchAttrsHtml + '>{{$select.selected.name}}</ui-select-match> \
-        <ui-select-choices repeat="person in people | filter: $select.search"> \
+        <ui-select-choices repeat="person in people | filter: $select.search' + choicesRepeatAttrsHtml + '"' + choicesAttrsHtml + '> \
           <div ng-bind-html="person.name | highlight: $select.search"></div> \
           <div ng-bind-html="person.email | highlight: $select.search"></div> \
         </ui-select-choices> \
@@ -1646,7 +1652,8 @@ describe('ui-select tests', function() {
 
     function createUiSelectMultiple(attrs) {
         var attrsHtml = '',
-            choicesAttrsHtml = '';
+            choicesAttrsHtml = '',
+			choicesRepeatAttrsHtml = '';
         if (attrs !== undefined) {
             if (attrs.disabled !== undefined) { attrsHtml += ' ng-disabled="' + attrs.disabled + '"'; }
             if (attrs.required !== undefined) { attrsHtml += ' ng-required="' + attrs.required + '"'; }
@@ -1656,12 +1663,13 @@ describe('ui-select tests', function() {
             if (attrs.taggingTokens !== undefined) { attrsHtml += ' tagging-tokens="' + attrs.taggingTokens + '"'; }
             if (attrs.inputId !== undefined) { attrsHtml += ' input-id="' + attrs.inputId + '"'; }
             if (attrs.groupBy !== undefined) { choicesAttrsHtml += ' group-by="' + attrs.groupBy + '"'; }
+			if (attrs.trackBy !== undefined) { choicesRepeatAttrsHtml += ' track by "' + attrs.trackBy +'"'; }
         }
 
         return compileTemplate(
             '<ui-select multiple ng-model="selection.selectedMultiple"' + attrsHtml + ' theme="bootstrap" style="width: 800px;"> \
                 <ui-select-match placeholder="Pick one...">{{$item.name}} &lt;{{$item.email}}&gt;</ui-select-match> \
-                <ui-select-choices repeat="person in people | filter: $select.search"' + choicesAttrsHtml + '> \
+                <ui-select-choices repeat="person in people | filter: $select.search' + choicesRepeatAttrsHtml + '"' + choicesAttrsHtml + '> \
                   <div ng-bind-html="person.name | highlight: $select.search"></div> \
                   <div ng-bind-html="person.email | highlight: $select.search"></div> \
                 </ui-select-choices> \
@@ -2683,6 +2691,117 @@ describe('ui-select tests', function() {
       var item = 2015;
 
       expect(highlight(item, query)).toBe('20<span class="ui-select-highlight">15</span>');
+    });
+  });
+  
+  describe('single selection with tagging function and tagging equals', function() {
+   it('should preserve the model if tagging is enabled on select single', function() {
+		scope.selection.selected = "I am not on the list of choices";
+		var el = createUiSelect({tagging: ''});
+		scope.$digest();
+		expect(scope.selection.selected).toEqual("I am not on the list of choices");
+    });
+  
+  it('should not call tagging function needlessly in single selection', function() {
+		scope.slowTaggingFunc = function (name) {
+			return {name: name};
+		};
+		spyOn(scope, 'slowTaggingFunc').and.callThrough();
+
+		var el = createUiSelect({tagging: 'slowTaggingFunc'});
+		scope.$digest();
+
+		openDropdown(el);
+	  
+		showChoicesForSearch(el, 'Foo');
+		expect(el.find('.ui-select-choices-row-inner').size()).toBe(6);
+
+		showChoicesForSearch(el, 'a');
+		expect(el.find('.ui-select-choices-row-inner').size()).toBe(9);
+
+		expect(scope.slowTaggingFunc.calls.count()).toBe(2);
+		expect(scope.slowTaggingFunc.calls.count()).not.toBe(15);
+    });
+	
+	it('should allow decline tags when tagging function returns null in single select mode', function() {
+		scope.taggingFunc = function (name) {
+			if (name == 'idontexist') return null;
+			return {
+				name: name,
+				email: name + '@email.com',
+				group: 'Foo',
+				age: 12
+			};
+		};
+		var el = createUiSelect({tagging: 'taggingFunc'});
+		scope.$digest();
+
+		openDropdown(el);
+		showChoicesForSearch(el, 'amalie');
+		expect(el.find('.ui-select-choices-row-inner').size()).toBe(2);
+		expect(el.scope().$select.items[0]).toEqual(jasmine.objectContaining({name: 'amalie', isTag: true}));
+		expect(el.scope().$select.items[1]).toEqual(jasmine.objectContaining({name: 'Amalie'}));
+
+		showChoicesForSearch(el, 'idoexist');
+		expect(el.find('.ui-select-choices-row-inner').size()).toBe(1);
+		expect(el.find('.ui-select-choices-row-inner').is(':contains(idoexist@email.com)')).toBeTruthy();
+
+		showChoicesForSearch(el, 'idontexist');
+		expect(el.find('.ui-select-choices-row-inner').size()).toBe(0);
+    });
+	
+	it('should allow creating tag in single select mode with tagging and group-by enabled', function() {
+      scope.taggingFunc = function (name) {
+        return {
+          name: name,
+          email: name + '@email.com',
+          group: 'Foo',
+          age: 12
+        };
+      };
+
+	  var el = createUiSelect({tagging: 'taggingFunc', groupBy: 'age'});
+	  scope.$digest();
+
+	  openDropdown(el);
+      showChoicesForSearch(el, 'amal');
+      expect(el.find('.ui-select-choices-row-inner').size()).toBe(2);
+      expect(el.scope().$select.items[0]).toEqual(jasmine.objectContaining({name: 'amal', email: 'amal@email.com', isTag: true}));
+      expect(el.scope().$select.items[1]).toEqual(jasmine.objectContaining({name: 'Amalie', email: 'amalie@email.com'}));
+    }); 
+	
+	it('should allow creating tag in single select mode with tagging-equals', function() {
+      scope.taggingFunc = function (name) {
+        return {
+		  name: name,
+          email: (name? name.toLowerCase() : '') + '@email.com',
+          group: 'Foo',
+          age: 12,
+		  createByTag: true
+        };
+      };
+	  
+	  scope.taggingEqualsFunc = function (item1, item2) {
+        return item1 && item2
+			&& item1.name === item2.name
+			&& item1.email === item2.email
+			&& item1.name === item2.name
+			&& item1.group === item2.group
+			&& item1.age === item2.age;
+      };
+
+      var el = createUiSelect({tagging: 'taggingFunc', taggingEquals: 'taggingEqualsFunc', trackBy: '$index'});
+	  scope.$digest();
+
+	  openDropdown(el);
+      showChoicesForSearch(el, 'amal');
+      expect(el.find('.ui-select-choices-row-inner').size()).toBe(2);
+      expect(el.scope().$select.items[0]).toEqual(jasmine.objectContaining({name: 'amal', email: 'amal@email.com', isTag: true}));
+      expect(el.scope().$select.items[1]).toEqual(jasmine.objectContaining({name: 'Amalie', email: 'amalie@email.com'}));
+	  
+	  showChoicesForSearch(el, 'Amalie');
+      expect(el.find('.ui-select-choices-row-inner').size()).toBe(1);
+      expect(el.scope().$select.items[0]).toEqual(jasmine.objectContaining({name: 'Amalie', email: 'amalie@email.com'}));
     });
   });
 
